55eaa719276e237382d761276f7e9e72ca1c8d1b
lurknyc <44297393+lurknyc@users.noreply.github.com>
2021-02-20 15:36:37 -0700

    Update passwordgen.py

--- passwordgen.py
+++ passwordgen.py
@@ -1,16 +1,17 @@
 
-
 import string
 
 import secrets
 
+length = input('How long do you need the password to be?\n')
+
 alphabet = string.ascii_letters + string.digits 
 
 while True:
-    password = ''.join(secrets.choice(alphabet) for i in range(15))
+    password = ''.join(secrets.choice(alphabet) for i in range(int(length)))
 
     if (sum(c.islower() for c in password) >=4
             and sum(c.isupper() for c in password) >=4
             and sum(c.isdigit() for c in password) >=4):
             print (password)
-            break
+            break 
